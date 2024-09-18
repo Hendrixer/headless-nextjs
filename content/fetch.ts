7b4d6e826e12d0e6896a059eb5ae16f0cf446a11
beizy <241zxy@gmail.com>
2024-09-18 15:45:46 -0400

    add our own content fetcher

--- content/fetch.ts
+++ content/fetch.ts
@@ -1,3 +1,24 @@
-export const contentGglFetcher = async () => {
-  const result = await fetch({})
+/**
+ * Since we got rid of the middleware contentlayer, this is our own data fetching middleware, using GraphQL queries. It is integrated with contentful GraphQL Content API https://www.contentful.com/developers/docs/references/graphql/
+ */
+export const contentGglFetcher = async (
+  query: string,
+  variables = {},
+  preview = false
+) => {
+  const result = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${
+          preview
+            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+            : process.env.CONTENTFUL_ACCESS_TOKEN
+        }`,
+      },
+      method: "POST",
+      body: JSON.stringify({ query, variables }),
+    }
+  )
 }
