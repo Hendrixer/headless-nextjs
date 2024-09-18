b85cd2d9223a0ba0e41ec321327d1c04ad6d6260
beizy <241zxy@gmail.com>
2024-09-18 15:01:29 -0400

    remove contentlayer middleware

---
+++ content/fetch.ts
@@ -0,0 +1,3 @@
+export const contentGglFetcher = async () => {
+  const result = await fetch({})
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = withContentlayer(nextConfig)
+module.exports = nextConfig
